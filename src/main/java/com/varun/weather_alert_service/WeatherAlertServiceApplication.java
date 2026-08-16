66d9932dd046f61fa489a48956c265be862399f2
Varun Kaushik <varunkaushik@Mac.fritz.box>
2026-08-16 14:52:58 +0200

    chore: bootstrap Spring Boot application

---
+++ src/main/java/com/varun/weather_alert_service/WeatherAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.varun.weather_alert_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAlertServiceApplication.class, args);
+	}
+
+}
